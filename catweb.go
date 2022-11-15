7fdbfd6df091553b4bac14721dd0ed7bac5fd535
adess-gitlab <adess@gitlab.com>
2022-11-15 11:01:10 +0100

    test

--- catweb.go
+++ catweb.go
@@ -49,7 +49,7 @@ func CatHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Choose random catpicture
 
-	catpic = 1
+	catpic := 1
 
 	if unleash.IsEnabled("catpic-random"){
 		catpic := random(2, 10)
