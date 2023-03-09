ef2532fa28a323dde6417673ab5b989a4586bf86
Alexander Dess <adess@gitlab.com>
2023-03-09 11:09:35 +0000

    add test

---
+++ .test/catweb_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestRandom(t *testing.T) {
+	min := 0
+	max := 10
+	result := random(min, max)
+	if result < min || result > max {
+		t.Errorf("Expected result between %d and %d, got %d", min, max, result)
+	}
+}
